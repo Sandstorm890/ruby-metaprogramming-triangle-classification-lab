e8bfbcaae61eaa543b0ef4528161e17b87c33000
zaynegoering <zaynegoering@gmail.com>
2020-11-07 11:46:19 -0800

    commit tree 22e5d9f6bdb11987339a16e0e7f41ea99f9a7671

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
+require "pry"
+
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @side_1 = a
+    @side_2 = b
+    @side_3 = c
+    @sides_list = [@side_1, @side_2, @side_3]
+  end
+
+  def kind
+    # binding.pry
+    if @sides_list.any?(nil) || @sides_list.any? {|side| side <= 0} || impossible_triangle?
+      begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      end
+    else
+      if @side_1 == @side_2 && @side_1 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+        :isosceles
+      elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+        :scalene
+      end
+    end
+
+  end
+
+  def impossible_triangle?
+
+    
+    # binding.pry
+    if (@side_1 + @side_2) <= @side_3
+      true
+    elsif (@side_1 + @side_3) <= @side_2
+      true
+    elsif (@side_2 + @side_3) <= @side_1
+      true
+    else
+      false
+    end
+    
+  end
+
+  class TriangleError < StandardError
+  end
 end
